875ab9da10bd02ee72ed61a0e4896b0a714b0473
Lukario45 <connorcs@ptd.net>
2013-08-17 01:12:50 -0400

    Temp Slotage on Days.java

--- src/com/lukario45/skyschedule/src/Days.java
+++ src/com/lukario45/skyschedule/src/Days.java
@@ -3,19 +3,22 @@ package com.lukario45.skyschedule.src;
 public class Days {
 	String name;
 	Boolean weekday;
-	public Days(String n, Boolean w){
+	String[] slots;
+	public Days(String n, Boolean w, String[] s){
 		this.name = n;
 		this.weekday = w;
+		this.slots = s;
 		
 	}
 	public static void createDays(){
-		Days mon = new Days("Monday",true);
-		Days tue = new Days("Tuesday",true);
-		Days wed = new Days("Wednsday",true);
-		Days thur = new Days("Thursday",true);
-		Days fri = new Days("Friday",true);
-		Days sat = new Days("Saturday",false);
-		Days sun = new Days("Sunday",false);
+		String[] allslots = {"a","b","c","d","e"};
+		Days mon = new Days("Monday",true, allslots );
+		Days tue = new Days("Tuesday",true,allslots);
+		Days wed = new Days("Wednsday",true,allslots);
+		Days thur = new Days("Thursday",true,allslots);
+		Days fri = new Days("Friday",true,allslots);
+		Days sat = new Days("Saturday",false,allslots);
+		Days sun = new Days("Sunday",false,allslots);
 	}
 
 }

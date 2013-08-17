773491aec62fe49b7ca1799a87dcf0f24ac06a05
Lukario45 <connorcs@ptd.net>
2013-08-17 09:40:29 -0400

    Updated Days

--- src/com/lukario45/skyschedule/src/Days.java
+++ src/com/lukario45/skyschedule/src/Days.java
@@ -3,22 +3,22 @@ package com.lukario45.skyschedule.src;
 public class Days {
 	String name;
 	Boolean weekday;
-	String[] slots;
-	public Days(String n, Boolean w, String[] s){
+	Boolean allSlots;
+	public Days(String n, Boolean w, Boolean a){
 		this.name = n;
 		this.weekday = w;
-		this.slots = s;
+		this.allSlots = a;
 		
 	}
 	public static void createDays(){
-		String[] allslots = {"a","b","c","d","e"};
-		Days mon = new Days("Monday",true, allslots );
-		Days tue = new Days("Tuesday",true,allslots);
-		Days wed = new Days("Wednsday",true,allslots);
-		Days thur = new Days("Thursday",true,allslots);
-		Days fri = new Days("Friday",true,allslots);
-		Days sat = new Days("Saturday",false,allslots);
-		Days sun = new Days("Sunday",false,allslots);
+		
+		Days mon = new Days("Monday",true, false );
+		Days tue = new Days("Tuesday",true, false);
+		Days wed = new Days("Wednsday",true, false);
+		Days thur = new Days("Thursday",true, false);
+		Days fri = new Days("Friday",true, false);
+		Days sat = new Days("Saturday",false, true);
+		Days sun = new Days("Sunday",false, true);
 	}
 
 }

9cfe9dbcae93301d802f0a9f3542758c96d9b6b4
Lukario45 <connorcs@ptd.net>
2013-08-17 09:30:14 -0400

    Updated Slots

--- src/com/lukario45/skyschedule/src/Slot.java
+++ src/com/lukario45/skyschedule/src/Slot.java
@@ -12,5 +12,14 @@ public class Slot {
 	public static void createSlots(){
 		Slot a = new Slot("A","6:00PM-7:30PM", true);
 		Slot b = new Slot("B","7:30PM-9:00PM", true);
+		Slot c = new Slot("C","9:00AM-10:00AM", true);
+		Slot d = new Slot("D","10:30AM-12:00PM", true);
+		Slot e = new Slot("E","12:00PM-1:30PM", true);
+		Slot f = new Slot("F","1:30PM-3:00PM", true);
+		Slot g = new Slot("G","3:00PM-4:30PM", true);
+		Slot h = new Slot("H","4:30PM-6:00PM", true);
+		Slot specMS = new Slot("SpecialMS","6:00PM-8:00PM", true);//Middle School Only
+		
+		
 	}
 }
